f78087b89462276cedd2886eccaaec8f22f7f60e
Daniel Rudzinski <danielrudzinski19@gmail.com>
2019-10-28 12:28:59 +0100

    Initial commit.

---
+++ src/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Core.Evidence
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Implementation/Commands/CreateEvidenceCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Evidence.Implementation.Commands
+{
+    public class CreateEvidenceCommand
+    {
+        public Guid? RiskRootId { get; set; }
+        public Guid? TreatmentRootId { get; set; }
+
+        public string Payload { get; set; }
+    }
+}
